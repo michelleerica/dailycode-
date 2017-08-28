f4d2c2ae4e4594f7c18768e8a86e6a0bfa8b4dfe
Ryan <shua.ruan@Gmail.com>
2017-08-28 16:43:35 +1000

    #2

---
+++ 2.Two Sum.js
@@ -0,0 +1,6 @@
+// You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+//
+// 这个题主要是考单向链接的基本知识。 !=和 !== 究竟有什么本质的区别
